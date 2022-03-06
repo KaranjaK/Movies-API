c9ea69cd7c5963ce782b671aeb458efabef0557c
KK <karanjakelvin77@gmail.com>
2022-03-06 09:29:18 +0300

    It will adjust WTForms for validation

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,8 @@
-from crypt import methods
 from flask import render_template, request, redirect, url_for
-
 from app.requests import get_movies, search_movie
 from . import main
 from ..models import Review
+from ..forms import ReviewForm
 
 @main.route('/')
 def index():
@@ -26,3 +25,30 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html', movies = searched_movies)
+
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movies(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
+
+@main.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movies(id)
+    title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
+
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
