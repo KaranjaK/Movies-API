254ef2426dfc81562693c4f74be776ab15de9e1d
KK <karanjakelvin77@gmail.com>
2022-03-06 08:52:58 +0300

    It will introduce a search mechanism

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 
-from app.requests import get_movies
+from app.requests import get_movies, search_movie
 from . import main
 from ..models import Review
 
@@ -10,4 +10,18 @@ def index():
     title = 'Home - Simply the best movies'
     popular_movies = get_movies('popular')
 
-    return render_template('index.html', message = message, title = title, popular = popular_movies)
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name=search_movie))
+    else:
+        return render_template('index.html', message = message, title = title, popular = popular_movies)
+
+@main.route('/search/<movie_name>')
+def search(movie_name):
+
+    movie_name_list = movie_name.split('')
+    movie_name_format = '+'.join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html', movies = searched_movies)
