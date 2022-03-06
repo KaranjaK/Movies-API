6bf5fe3544f76c23c8fe4fff8f39c23d8e15aa9c
KK <karanjakelvin77@gmail.com>
2022-03-06 12:43:11 +0300

    It will configure the database shell

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
-from app import create_app
+from app import create_app, db
+from app.models import User
 from flask_script import Manager, Server
 
 app = create_app('development')
@@ -14,3 +15,10 @@ def test():
 
 if __name__ == '__main__':
     manager.run()
+
+@manager.shell
+def make_shell_context():
+    return dict(app =app, db = db, User = User)
+if __name__ == '__main__':
+    manager.run()
+    
